dff163fa93e43650a88a19bd311dcc2d585774bc
Mohamed Nisar <mohamed.nisar@mavapartners.com>
2018-04-12 11:27:28 +0530

    fixing

--- functions/utilities.js
+++ functions/utilities.js
@@ -73,7 +73,7 @@ exports.getGreetingTime = function(m) {
   if(!m || !m.isValid()) { return; } //if we can't find a valid or filled moment, we return.
   
   var split_afternoon = 12 //24hr time to split the afternoon
-  var split_evening = 17 //24hr time to split the evening
+  var split_evening = 16 //24hr time to split the evening
   var currentHour = parseFloat(m.format("HH"));
   
   if(currentHour >= split_afternoon && currentHour <= split_evening) {
